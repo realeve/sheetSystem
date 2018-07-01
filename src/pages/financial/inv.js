1b4bddcfa52aa743745ac08397caebd8c42ae878
musicoooool <musicoooool@gmail.com>
2018-07-02 00:19:57 +0800

    期间处理

--- src/pages/financial/inv.js
+++ src/pages/financial/inv.js
@@ -5,10 +5,19 @@ import { DatePicker } from "antd";
 import styles from "./index.less";
 import moment from "moment";
 import "moment/locale/zh-cn";
+import { getPeriodid } from "./services/db";
 moment.locale("zh-cn");
+
+const MonthPicker = DatePicker.MonthPicker;
 
 function Tables({ dispatch, dateRange, loading, dataSource }) {
   const onDateChange = async (dates, dateStrings) => {
+    let monthB = moment(dateStrings, 'YYYY-MM').format("MM-YY");
+    let monthA = moment(dateStrings, 'YYYY-MM').add(-1, "months").format("MM-YY");
+    console.log(monthA, monthB);
+    let periodA = await getPeriodid(monthB);
+    let periodB = await getPeriodid(monthB);
+    console.log(periodA, periodB);
     await dispatch({
       type: "financial/setStore",
       payload: { dateRange: [dateStrings, dateStrings] }
@@ -19,13 +28,19 @@ function Tables({ dispatch, dateRange, loading, dataSource }) {
     });
   };
 
+  const disabledDate = (current) => {
+    // Can not select days before today and today
+    return current && current >= moment().endOf('day');
+  }
+
   const DateRangePicker = () => (
     <div>
       <label className={styles.labelDesc}>查询期间:</label>
-      <DatePicker
+      <MonthPicker
         allowClear={false}
-        format="YYYY-MM-DD"
+        format="YYYY-MM"
         onChange={onDateChange}
+        disabledDate={disabledDate}
         defaultValue={moment(dateRange[1])}
       />
     </div>
